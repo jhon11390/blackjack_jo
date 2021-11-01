c9213e525e01290292eb25502359d8c4ae422714
jhon11390 <meljhon2@gmail.com>
2021-11-01 04:33:57 -0500

    agrega animacion de inicio de juego

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,53 @@
 import { Button } from 'react-bootstrap';
 
+
 function Table() {
+  
+  
+
+
+  const iniciarJuego = (e) => {
+    const cartas = document.querySelectorAll('.cartaPoker')
+    
+    for (let i = 0; i < cartas.length; i++) {
+      cartas[i].style.transform = 'rotate(0deg)'
+    }
+    
+    e.target.style.display = 'none' 
+
+    setTimeout(() => {
+      for (let i = 0; i < cartas.length; i++) {
+        cartas[i].style.top = '25px'
+      }
+      setTimeout(() => {
+        for (let i = 0; i < 4; i++) {
+          if(i === 0){
+            cartas[i].style.top = '-120px'
+            cartas[i].style.right = '100px'
+          } else if (i === 1){
+            cartas[i].style.top = '-120px'
+            cartas[i].style.left = '100px'
+          } else if (i === 2){
+            cartas[i].style.top = '170px'
+            cartas[i].style.right = '100px'
+          } else if (i === 1){
+          } else {
+            cartas[i].style.top = '170px'
+            cartas[i].style.left = '100px'
+          }
+          
+        }
+      }, 1500);
+    }, 1500);
+
+    
+  }
+
+
   return (
   
     <div style={{width: "100%"}}>
+      
       <style type="text/css">
         {`
           .cartaPoker{
@@ -11,6 +55,7 @@ function Table() {
             background-repeat: no-repeat;
             background-size: 95%;
             background-position: center;
+            transition: all .7s;
           }
 
           .cartaPoker:nth-child(2){
@@ -37,11 +82,12 @@ function Table() {
             transform-origin: center 250px;
             transform: rotate(300deg);
           }
+
         `}
       </style>
       <div style={{border: "1px solid red", height: "100vh", display: "flex", justifyContent: "center"}}>
         <div style={{width: "200px", height: "200px", margin: "auto", border: "1px solid green", borderRadius: "50%", position: "relative", display: "flex"}}>
-          <Button variant="dark" style={{margin: "auto", display: "block", width: "150px"}}>Iniciar Juego</Button>
+          <Button id="btnGame" variant="dark" style={{margin: "auto", display: "block", width: "150px"}} onClick={iniciarJuego}>Iniciar Juego</Button>
           <div className="cartaPoker" style={{width: "100px", height: "150px", position: "absolute", top: "-150px", right: "50px"}}></div>
           <div className="cartaPoker" style={{width: "100px", height: "150px", position: "absolute", top: "-150px", right: "50px"}}></div>
           <div className="cartaPoker" style={{width: "100px", height: "150px", position: "absolute", top: "-150px", right: "50px"}}></div>
